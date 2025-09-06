a038fd83336d45ce8ddeba01804cae33d800ac1b
Lee-Way Wang <lee.way56@comcast.net>
2025-09-05 23:42:48 -0700

    - Update sky130hd/gcd/constraint.sdc to account for two clocks - Create specify_files_for_sdc.tcl

---
+++ flow/platforms/sky130hd/specify_files_for_sdc.tcl
@@ -0,0 +1,4 @@
+read_lib platforms/sky130hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+read_db results/sky130hd/gcd/base/6_final.odb
+read_spef results/sky130hd/gcd/base/6_final.spef
+read_sdc results/sky130hd/gcd/base/6_final.sdc
