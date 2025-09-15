5de4bea9709864de51d69328b0dfa2fee29c86fb
Lee-Way Wang <lee.way56@comcast.net>
2025-09-14 21:50:37 -0700

    - Delete dff_with_loop_to_latch.v - Update and clean up dff2latch.v

--- flow/platforms/sky130hd/dff2latch.v
+++ flow/platforms/sky130hd/dff2latch.v
@@ -1,8 +1,54 @@
-// This file is inspired by
-// https://github.com/The-OpenROAD-Project/OpenROAD-flow-scripts/blob/master/flow/platforms/sky130hd/cells_latch_hd.v
-// That is also similar to this file:
-// /home/leeway64/.volare/sky130A/libs.tech/openlane/sky130_fd_sc_hd/latch_map.v
-// Refer to this page for a list of all the cells in the SKY130 PDK
+// Refer to these two files for more information on mapping:
+//  OpenROAD-flow-scripts/flow/platforms/sky130hd/cells_latch_hd.v
+//  $env(PDK_ROOT)/sky130A/libs.tech/openlane/sky130_fd_sc_hd/latch_map.v
+
+
+
+module \$_SDFFE_PN0P_ (input D, C, R, E, output Q);
+    wire temp_q;
+    sky130_fd_sc_hd__dlxtp_1 _TECHMAP_REPLACE_ (
+        .GATE(C && E),
+        .D(D),
+        .Q(temp_q)
+    );
+
+    always @(*) begin
+        if (R && C) begin
+            Q <= 1'b0;
+        end else begin
+            Q <= temp_q;
+        end
+    end
+
+endmodule
+
+module \$_SDFFE_PP0P_ (input D, C, R, E, output Q);
+    wire temp_q;
+    sky130_fd_sc_hd__dlxtp_1 _TECHMAP_REPLACE_ (
+        .GATE(C && E),
+        .D(D),
+        .Q(temp_q)
+    );
+    
+    always @(*) begin
+        if (!R && C) begin
+            Q <= 1'b0;
+        end else begin
+            Q <= temp_q;
+        end   
+    end
+endmodule
+
+module \$_DFFE_PP0P_ (input D, C, R, E, output Q);
+    sky130_fd_sc_hd__dlrtp_1 _TECHMAP_REPLACE_ (
+        .GATE(C && E),
+        .RESET_B(!R),
+        .D(D),
+        .Q(Q)
+    );
+endmodule
+
+
 
 // edfxtp and dlxtp only have a size of 1
 module sky130_fd_sc_hd__edfxtp_1
@@ -120,73 +166,4 @@ module sky130_fd_sc_hd__dfxtp
         .GATE(CLK),
         .Q(Q)
     );
-endmodule
-
-
-
-// ABC can't map sky130_fd_sc_hd__dfbbn, sky130_fd_sc_hd__dfbbp, sky130_fd_sc_hd__dfrbp,
-// sky130_fd_sc_hd__dfsbp, and sky130_fd_sc_hd__dfxbp because ABC can't map multi-output gates.
-// See the following for more details:
-// https://github.com/YosysHQ/yosys/issues/3631#issuecomment-1387506461
-// https://github.com/YosysHQ/yosys/discussions/5195#discussioncomment-13760120
-
-module sky130_fd_sc_hd__dfrbp_1
-(
-    input CLK,
-    input D,
-    input RESET_B,
-    output Q,
-    output Q_N
-);
-    sky130_fd_sc_hd__dlrbp_1 _TECHMAP_REPLACE_
-    (
-        .RESET_B(RESET_B),
-        .D(D),
-        .GATE(CLK),
-        .Q(Q),
-        .Q_N(Q_N)
-    );
-endmodule
-
-module sky130_fd_sc_hd__dfxbp_1
-(
-    input CLK,
-    input D,
-    output Q,
-    output Q_N
-);
-    sky130_fd_sc_hd__dlxbp_1 _TECHMAP_REPLACE_
-    (
-        .D(D),
-        .GATE(CLK),
-        .Q(Q),
-        .Q_N(Q_N)
-    );
-endmodule
-
-module sky130_fd_sc_hd__dfsbp_2
-(
-    input CLK,
-    input D,
-    input SET_B,
-    output Q,
-    output Q_N
-);
-    // Similarly, the SKY130 PDK doesn't have a latch with asynchronous set and complementary
-    // outputs, so we need to create one here
-    wire temp_Q;
-
-    wire D_or_set, gate_or_set;
-    assign D_or_set = D || ~SET_B;
-    assign gate_or_set = CLK || ~SET_B;
-
-    sky130_fd_sc_hd__dlxtp_2 _TECHMAP_REPLACE_ (
-        .D(D_or_set),
-        .GATE(gate_or_set),
-        .Q(temp_Q)
-    );    
-
-    assign Q = temp_Q;
-    assign Q_N = ~temp_Q;
-
 endmodule
