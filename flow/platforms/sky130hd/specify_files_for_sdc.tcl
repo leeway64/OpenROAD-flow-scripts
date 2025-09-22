076597df293c98b3b5d21e54f411fa39b8bd4976
Lee-Way Wang <lee.way56@comcast.net>
2025-09-21 23:36:09 -0700

    Create a base_dir variable in specify_files_for_sdc.tcl

--- flow/platforms/sky130hd/specify_files_for_sdc.tcl
+++ flow/platforms/sky130hd/specify_files_for_sdc.tcl
@@ -1,7 +1,8 @@
 set design gcd
+set base_dir "/OpenROAD-flow-scripts/flow"
 
-read_lib platforms/sky130hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+read_lib $base_dir/platforms/sky130hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 
-read_db results/sky130hd/$design/base/6_final.odb
-read_spef results/sky130hd/$design/base/6_final.spef
-read_sdc designs/sky130hd/$design/two_phase_clk_constraint.sdc
+read_db $base_dir/results/sky130hd/$design/base/6_final.odb
+read_spef $base_dir/results/sky130hd/$design/base/6_final.spef
+read_sdc $base_dir/designs/sky130hd/$design/two_phase_clk_constraint.sdc
