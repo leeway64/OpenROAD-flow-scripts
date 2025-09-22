eb09689d0db0287e399751712df8fefb15a06be8
Lee-Way Wang <lee.way56@comcast.net>
2025-09-21 17:08:05 -0700

    - Update default_constraint.sdc for aes, gcd, and riscv32i - Use two_phase_clk_constraint.sdc in specify_files_for_sdc.tcl

--- flow/platforms/sky130hd/specify_files_for_sdc.tcl
+++ flow/platforms/sky130hd/specify_files_for_sdc.tcl
@@ -4,4 +4,4 @@ read_lib platforms/sky130hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 
 read_db results/sky130hd/$design/base/6_final.odb
 read_spef results/sky130hd/$design/base/6_final.spef
-read_sdc designs/sky130hd/$design/constraint.sdc
+read_sdc designs/sky130hd/$design/two_phase_clk_constraint.sdc
